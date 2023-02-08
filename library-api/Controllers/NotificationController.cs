4eb0878013a5b264ace4ec41a601ad2c8cadda33
Winner Emetuche <45737118+viandmarket25@users.noreply.github.com>
2023-02-09 01:01:51 +0800

    m

--- library-api/Controllers/NotificationController.cs
+++ library-api/Controllers/NotificationController.cs
@@ -54,61 +54,93 @@ public class NotificationsController : ControllerBase
     }
     // ::::::::::::::::::::::: get a book information
     [HttpGet("notification")]
-    public Notification GetNotification(int Id)
+    public  IActionResult GetNotification(int Id)
     {
-        Console.WriteLine("get book");
-        // :::::::::::::::: Create a list of books
-        MySqlCommand command;
-        //string query = "SELECT * FROM books WHERE books.BOOK_ID== @Id;";
-        command  = new MySqlCommand(
-            "SELECT * FROM books WHERE  books.BOOK_ID=@Id",this.connection);
-        command.Prepare();
-        command.Parameters.AddWithValue("@Id", Id);
-       
-        //using var command = new MySqlCommand(query,this. connection);
+        Console.WriteLine("get notification");
+        // :::::::::::::::: get a notification s
+        List<dynamic> notifications=new List<dynamic>();
+        string query = "SELECT * FROM notifications;";
+        using var command = new MySqlCommand(query,this.connection);
         using var reader = command.ExecuteReader();
         if (reader.Read()){
             // :::::::::::::: Create a book Object to hold db data
-            var notification=new Notification();
-           
+            var notification = new Notification();
+            notification.Id = reader.GetInt32(0);
+            notification.Title = reader.GetString(1);
+            notification.Body =reader.GetString(2);
+            notification.IsRead = reader.GetInt32(0);
+            notification.CustomerId = reader.GetString(1);
             Console.WriteLine(notification);
-            return notification;
-        }else return new Notification();
+            notifications.Add(notification);
+            requestResponse=new RequestResponse{
+                Message="success",
+                Result=  notifications,
+                Status="success",
+                Code="200"
+            };
+            return Ok(requestResponse);
+        }else{
+            requestResponse=new RequestResponse{
+                Message="failed, no data received",
+                Result=  notifications,
+                Status="failed",
+                Code="200"
+            };
+            return Ok(requestResponse);
+        }
     }
     // ::::::::::::::::::::: add book information
     [HttpPost("add-notification")]
-    public Notification AddNotification(int Id)
+    public  IActionResult AddNotification(Notification  notification)
     {
-        Console.WriteLine("get book");
-        // :::::::::::::::: Create a list of books
-        return new Notification();
+        // :::::::::::::::::
+        Console.WriteLine("add new notification");
+        var queryStatement = "INSERT INTO shelves( \n"+
+        "ID, TITLE, BODY, IS_READ, CUSTOMER_ID,DATE_,TIME_)\n"+
+        " VALUES (@Id,@Name,@Description)";
+        // :::::::::::
+        MySqlCommand command;
+        command = new MySqlCommand(queryStatement,this.connection);
+        command.Prepare();
+        command.Parameters.AddWithValue("@Id", null);
+        command.Parameters.AddWithValue("@Title", notification.Name);
+        command.Parameters.AddWithValue("@Body", notification.Description);
+        command.Parameters.AddWithValue("@IsRead", null);
+        command.Parameters.AddWithValue("@CustomerId", notification.Name);
+        command.Parameters.AddWithValue("@Date_", notification.Description);
+        command.Parameters.AddWithValue("@Time_", notification.Description);
+
+
+        command.ExecuteNonQuery();
+        // :::::::::::::::: 
+        requestResponse=new RequestResponse{
+                Message="Successful, Record created",
+                Result= new List<dynamic>(),
+                Status="success",
+                Code="200"
+        };
+        return Ok(requestResponse);
+
+
  
     }
-    // ::::::::::::::::::::: update book information
+    // ::::::::::::::::::::: update notification read
     [HttpPut ("read-notification")]
-    public Notification ReadNotification(int Id)
+    public  IActionResult ReadNotification(int Id)
     {
-        Console.WriteLine("get book");
+        Console.WriteLine("read notification");
         // :::::::::::::::: Create a list of books
         return new Notification();
     
     }
     // :::::::::::::::::: delete notification
     [HttpDelete("customer-delete-notification/{Id}")]
-    public Notification CustomerDeleteBook(int Id)
+    public  IActionResult CustomerDeleteBook(int Id)
     {
-        Console.WriteLine("get book");
+        Console.WriteLine("delete notification");
         // :::::::::::::::: Create a list of books
         return new Notification();
      
     }
-    // :::::::::::::::::: admin delete notification
-    [HttpDelete("admin-delete-notification/{Id}")]
-    public Notification AdminDeleteNotification(int Id)
-    {
-        Console.WriteLine("get book");
-        // :::::::::::::::: Create a list of books
-        return new Notification();
    
-    }
 }
